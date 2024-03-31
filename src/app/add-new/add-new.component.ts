3d5a43cebc054bd85d20f9c7085ec6edd13f05e2
Aditya Patil <aditya@customtechnologies.biz>
2024-03-31 23:52:18 +0530

    Integrated Material Design
    Added material design elements like Dialog box, button

---
+++ src/app/add-new/add-new.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {
+  MatDialogModule,
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-add-new',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
+  ],
+  templateUrl: './add-new.component.html',
+  styleUrl: './add-new.component.scss',
+})
+export class AddNewComponent {}
--- src/app/header-section/header-section.component.ts
+++ src/app/header-section/header-section.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { AddNewComponent } from '../add-new/add-new.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-section',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
   templateUrl: './header-section.component.html',
-  styleUrl: './header-section.component.scss'
+  styleUrl: './header-section.component.scss',
 })
 export class HeaderSectionComponent {
+  constructor(public dialog: MatDialog) {}
 
+  open() {
+    this.dialog.open(AddNewComponent);
+
+    // let dialogRef = dialog.open(UserProfileComponent, {
+    //   height: '400px',
+    //   width: '600px',
+    // });
+  }
 }
